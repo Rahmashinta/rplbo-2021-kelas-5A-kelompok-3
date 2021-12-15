fdb60872b9e9c4d20bf0ccd68d1e314399896d33
Rahmashinta <11950125168@students.uin-suska.ac.id>
2021-12-15 15:59:37 +0700

    first commit

--- app/Controllers/Pengguna.php
+++ app/Controllers/Pengguna.php
@@ -4,35 +4,12 @@ namespace App\Controllers;
 
 class Pengguna extends BaseController
 {
-    public function pengguna()
+    public function index()
     {
 
         $data = [
             'title' => 'Sistem Informasi Pelayanan Surat Menyurat'
         ];
         return view('welcome.php', $data);
-    }
-
-    public function suratmasuk()
-    {
-        $data = [
-            'title' => 'Sistem Informasi Pelayanan Surat Menyurat'
-        ];
-        return view('suratmasuk/index', $data);
-    }
-    public function contact()
-    {
-        $data = [
-            'title' => 'Sistem Informasi Pelayanan Surat Menyurat'
-        ];
-        return view('pages/contact', $data);
-    }
-    public function beranda()
-    {
-
-        $data = [
-            'title' => 'Sistem Informasi Pelayanan Surat Menyurat'
-        ];
-        return view('pages/beranda', $data);
     }
 }
